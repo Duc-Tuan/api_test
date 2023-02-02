ab2245f4f227ecfffdbc554023af9719869017d7
Duc-tuan <phamductuan26032002@gmail.com>
2023-02-02 16:16:50 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin12345@cluster0.fkjkxce.mongodb.net/customerdb?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connection success"))
+  .catch((error) => console.error(`connection failed ${error}`));
+
+app.get("/", (req, res) => {
+  return res.send("SERVER ON");
+});
+
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
