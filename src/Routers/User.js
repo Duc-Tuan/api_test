3c9051af64354fdee96415067a13e79c9044accf
Duc-tuan <phamductuan26032002@gmail.com>
2023-02-03 14:42:03 +0700

    update

--- src/Routers/User.js
+++ src/Routers/User.js
@@ -5,7 +5,8 @@ const UserController = require("../Controllers/user");
 const uploadFile = require("../middlewares/uploadFile");
 const decentralization = require("../DecentralizationAdmin");
 
-routerUsers.route("/").post(decentralization.decentralization__Users, UserController.index);
+routerUsers.route("/")
+  .get(decentralization.decentralization__Users, UserController.index);
 
 routerUsers.route("/login").post(UserController.postLoginUser);
 
