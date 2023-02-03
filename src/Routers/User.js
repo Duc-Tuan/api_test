3624f5400eacc4be754625e0fef1c43afa02d104
Duc-tuan <phamductuan26032002@gmail.com>
2023-02-03 14:43:51 +0700

    update

--- src/Routers/User.js
+++ src/Routers/User.js
@@ -2,48 +2,48 @@ const express = require("express");
 const routerUsers = express.Router();
 
 const UserController = require("../Controllers/user");
-const uploadFile = require("../middlewares/uploadFile");
+// const uploadFile = require("../middlewares/uploadFile");
 const decentralization = require("../DecentralizationAdmin");
 
 routerUsers.route("/")
   .get(decentralization.decentralization__Users, UserController.index);
 
-routerUsers.route("/login").post(UserController.postLoginUser);
+// routerUsers.route("/login").post(UserController.postLoginUser);
 
-routerUsers.route("/loginUser").post(UserController.getloginUser);
+// routerUsers.route("/loginUser").post(UserController.getloginUser);
 
-routerUsers.route("/register").post(UserController.newUser);
+// routerUsers.route("/register").post(UserController.newUser);
 
-routerUsers.route("/resetPass").post(UserController.resetPass);
+// routerUsers.route("/resetPass").post(UserController.resetPass);
 
-routerUsers
-  .route("/:userID")
-  .get(UserController.getUserById)
-  .put(UserController.replaceUser)
-  .patch(uploadFile.single("avatar"), UserController.updateUser)
-  .delete(decentralization.decentralization__Users ,UserController.deleteUser);
+// routerUsers
+//   .route("/:userID")
+//   .get(UserController.getUserById)
+//   .put(UserController.replaceUser)
+//   .patch(uploadFile.single("avatar"), UserController.updateUser)
+//   .delete(decentralization.decentralization__Users ,UserController.deleteUser);
 
-routerUsers
-  .route("/:userID/carts")
-  .get(UserController.getUserProduct)
-  .post(UserController.newUseProduct);
+// routerUsers
+//   .route("/:userID/carts")
+//   .get(UserController.getUserProduct)
+//   .post(UserController.newUseProduct);
 
-routerUsers
-  .route("/:userID/pay")
-  .get(UserController.getUserPay)
-  .post(UserController.newUsePay);
+// routerUsers
+//   .route("/:userID/pay")
+//   .get(UserController.getUserPay)
+//   .post(UserController.newUsePay);
 
-routerUsers
-  .route("/:userID/history")
-  .get(UserController.getUserHistory)
-  .post(UserController.newUseHistory);
+// routerUsers
+//   .route("/:userID/history")
+//   .get(UserController.getUserHistory)
+//   .post(UserController.newUseHistory);
 
-routerUsers.route("/:userID/history/:statusName")
-  .get(UserController.getUserHistoryStatus);
+// routerUsers.route("/:userID/history/:statusName")
+//   .get(UserController.getUserHistoryStatus);
 
-routerUsers
-  .route("/:userID/carts/:productID")
-  .post(UserController.postProductUser)
-  .delete(UserController.deleteCartItem);
+// routerUsers
+//   .route("/:userID/carts/:productID")
+//   .post(UserController.postProductUser)
+//   .delete(UserController.deleteCartItem);
 
 module.exports = routerUsers;
