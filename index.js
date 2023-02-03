30daec09ecc62e86813ab0723baae934fa104837
Duc-tuan <phamductuan26032002@gmail.com>
2023-02-03 14:24:55 +0700

    update

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const users = require("./src/Routers/user");
 const products = require("./src/Routers/product");
-
-// const users = require("./Routers/user");
-// const products = require("./Routers/product");
-// const searchProducts = require("./Routers/searchProducts");
-// const banner = require("./Routers/banner");
-// const category = require("./Routers/Category");
-// const promotion = require("./Routers/Promotion");
-// const comments = require("./Routers/Comments");
-// const userAdmin = require("./Routers/Admin/User");
+const searchProducts = require("./src/Routers/searchProducts");
+const banner = require("./src/Routers/banner");
+const category = require("./src/Routers/Category");
+const promotion = require("./src/Routers/Promotion");
+const comments = require("./src/Routers/Comments");
+const userAdmin = require("./src/Routers/Admin/User");
 
 mongoose
   .connect(
@@ -26,12 +24,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/products", products);
-// app.use("/users", users);
-// app.use("/search/products", searchProducts);
-// app.use("/banner", banner);
-// app.use("/category", category);
-// app.use("/promotion", promotion);
-// app.use("/comments", comments);
+app.use("/users", users);
+app.use("/search/products", searchProducts);
+app.use("/banner", banner);
+app.use("/category", category);
+app.use("/promotion", promotion);
+app.use("/comments", comments);
 app.use("/src/Images/avatarUsers", express.static("src/Images/avatarUsers"));
 app.use(
   "/src/Images/ImageProducts",
@@ -41,7 +39,7 @@ app.use("/src/Images/ImageBanner", express.static("src/Images/ImageBanner"));
 app.use("/src/Images/Promotion", express.static("src/Images/Promotion"));
 
 // Admin
-// app.use("/userAdmin", userAdmin);
+app.use("/userAdmin", userAdmin);
 
 // Start the server
 const port = process.env.PORT || 3000;
