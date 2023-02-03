699ad20c154f539f1e74a435003b53d2ed34329b
Duc-tuan <phamductuan26032002@gmail.com>
2023-02-03 14:52:21 +0700

    update

--- src/Controllers/user.js
+++ src/Controllers/user.js
@@ -1,4 +1,4 @@
-const User = require("../Models/user");
+// const User = require("../Models/user");
 // const Product = require("../Models/Product");
 // const Carts = require("../Models/Cart");
 // const HistoryBuy = require("../Models/HistoryBuy");
@@ -10,13 +10,14 @@ const User = require("../Models/user");
 // const nodemailer = require("nodemailer");
 
 const index = (req, res, next) => {
-  User.find()
-    .then((users) => {
-      return res.status(200).json({
-        users,
-      });
-    })
-    .catch((err) => next(err));
+  // User.find()
+  //   .then((users) => {
+  //     return res.status(200).json({
+  //       users,
+  //     });
+  //   })
+  //   .catch((err) => next(err));
+  return res.send("SERVER ON");
 };
 
 // const getloginUser = (req, res, next) => {
